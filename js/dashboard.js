e78f188096d3fcf2a2255b3d6063d03346609c93
Alex Roye <alex@creativefaze.com>
2017-07-26 12:31:06 +0500

    Google pie chart with actual data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,21 +1,33 @@
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+function drawChart() {
 
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Published',     11],
-          ['Unpublished',      2],
-        ]);
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Published',   3],
+    ['Unpublished', 0],
+  ]);
 
-        var options = {
-          animation: {
-            duration: 2000
-          }
-        };
+  var options = {
+    title: 'Nodes status',
+    is3D: true,
+  };
 
-        var chart = new google.visualization.PieChart(document.getElementById('nodes-chart'));
+  var chart = new google.visualization.PieChart(document.getElementById('nodes-chart'));
 
-        chart.draw(data, options);
-      }
+  setInterval(function(){
+    jQuery.getJSON('/admin/dashboard/data', function(result){
+      data.setValue(0, 1, result.nodes.published);
+      chart.draw(data, options);
+    });
+  }, 5000);
+
+  setInterval(function(){
+    jQuery.getJSON('/admin/dashboard/data', function(result){
+      data.setValue(1, 1, result.nodes.unpublished);
+      chart.draw(data, options);
+    });
+  }, 5000);
+  chart.draw(data, options);
+}
