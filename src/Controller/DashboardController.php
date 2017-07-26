4a4315b897b3fd62ecfda845fb5f99108b08e96e
Alex Roye <alex@creativefaze.com>
2017-07-26 16:29:50 +0500

    Online users

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -15,10 +15,14 @@ class DashboardController extends ControllerBase {
     $events   = \Drupal::entityQuery('node')->condition('type','events')->execute();
     $pages   = \Drupal::entityQuery('node')->condition('type','page')->execute();
     $articles   = \Drupal::entityQuery('node')->condition('type','article')->execute();
+    $online = db_query('SELECT uid FROM {sessions} WHERE uid != 0')->fetchAllAssoc('uid');
     $data = array('nodes' => [
       'events'   => count($events),
       'pages' => count($pages),
       'articles' => count($articles),
+    ],
+    'users' => [
+      'online' => count($online),
     ]);
     return new JsonResponse($data);
   }
