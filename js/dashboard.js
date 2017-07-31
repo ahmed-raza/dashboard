348b3e9330fee812e1e3a2995c01f8d584921cee
Alex Roye <alex@creativefaze.com>
2017-07-31 15:25:27 +0500

    Bar chart for showing users data

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,24 +11,8 @@ function drawChart() {
   ]);
 
   var options = {
-    title: 'Nodes status',
-    titleTextStyle: {
-      color: 'limegreen'
-    },
-    tooltip: {
-      textStyle: {
-        color: 'limegreen'
-      }
-    },
-    legend: {
-      textStyle: {
-        color: 'limegreen'
-      }
-    },
+    height: 300,
     is3D: true,
-    backgroundColor: {
-      fill: '#333'
-    },
   };
 
   var chart = new google.visualization.PieChart(document.getElementById('nodes-chart'));
@@ -90,6 +74,40 @@ function drawGaugeChart() {
       if (result.users.online > 7) {
         jQuery('strong').addClass('red');
       }
+    });
+  }, 3000);
+}
+
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawBarChart);
+function drawBarChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ["Element", "Total", { role: "style" } ],
+    ["Admins", 0, "red"],
+    ["Moderators", 0, "gold"],
+    ["Authenticated", 0, "silver"],
+  ]);
+
+  var view = new google.visualization.DataView(data);
+
+  var options = {
+    width: 600,
+    height: 400,
+    bar: {groupWidth: "100%"},
+    legend: { position: "none" },
+    animation: {
+      duration: 1000
+    }
+  };
+  var chart = new google.visualization.BarChart(document.getElementById("barchart"));
+
+  setInterval(function() {
+    jQuery.getJSON('/admin/dashboard/data', function(result){
+      data.setValue(0, 1, result.users.administrator);
+      data.setValue(1, 1, result.users.moderator);
+      data.setValue(2, 1, result.users.authenticated);
+      chart.draw(view, options);
     });
   }, 3000);
 }
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -22,8 +22,58 @@ class DashboardController extends ControllerBase {
       'articles' => count($articles),
     ],
     'users' => [
+      'total_users' => $this->getUserByRole(),
+      'administrator' => $this->getUserByRole('administrator'),
+      'authenticated' => $this->getUserByRole('authenticated'),
+      'moderator' => $this->getUserByRole('moderator'),
       'online' => count($online),
     ]);
     return new JsonResponse($data);
+  }
+
+  private function getUserByRole($key) {
+    switch ($key) {
+      case 'administrator':
+          $users = user_load_multiple();
+          $admins = [];
+          unset($users[0]);
+          foreach ($users as $user) {
+            if (in_array("administrator", $user->getRoles())) {
+              array_push($admins, $user);
+            }
+          }
+          return count($admins);
+        break;
+
+      case 'authenticated':
+          $users = user_load_multiple();
+          $authenticated = [];
+          unset($users[0]);
+          foreach ($users as $user) {
+            if (in_array("authenticated", $user->getRoles())) {
+              array_push($authenticated, $user);
+            }
+          }
+          return count($authenticated);
+        break;
+
+      case 'moderator':
+          $users = user_load_multiple();
+          $moderator = [];
+          unset($users[0]);
+          foreach ($users as $user) {
+            if (in_array("moderator", $user->getRoles())) {
+              array_push($moderator, $user);
+            }
+          }
+          return count($moderator);
+        break;
+      
+      default:
+        $users = user_load_multiple();
+        unset($users[0]);
+        return count($users);
+        break;
+    }
   }
 }
